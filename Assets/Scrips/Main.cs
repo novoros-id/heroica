ce32acdbf9dcf94abd28977a4b60652fd1a1945e
novoros-id <novoros-id@inbox.ru>
2021-01-13 21:05:39 +0300

    pr hod

--- Assets/Scrips/Main.cs
+++ Assets/Scrips/Main.cs
@@ -7,6 +7,14 @@ public class Main : MonoBehaviour
     public float pl_fl_x = 1.2f;
     public float pl_fl_z = 0.8f;
     private int current_move = 1;
+    public GameObject[] player;
+    public GameObject[] pr_hod;
+    public GameObject selected1;
+
+    public void Start()
+    {
+        move_priznak_step();
+    }
 
     public int get_current_move()
     {
@@ -15,9 +23,43 @@ public class Main : MonoBehaviour
 
     public void set_current_move()
     {
+        // сдвинем ход 
+
         if (current_move == 4)
             current_move = 1;
         else
             current_move += 1;
+
+        // очистим значок хода
+
+        pr_hod = GameObject.FindGameObjectsWithTag("pr_hod");
+
+        for (int b = 0; b < pr_hod.Length; b++)
+        {
+            Destroy(pr_hod[b]);
+        }
+
+        // передвинем знак хода
+        move_priznak_step();
+
+    }
+
+    public void move_priznak_step()
+    {
+   
+        player = GameObject.FindGameObjectsWithTag("Player");
+
+        for (int i = 0; i < player.Length; i++)
+        {
+
+            Player_ pl_script = player[i].GetComponent<Player_>();
+
+            if (pl_script.step_move == current_move)
+            {
+                Instantiate(selected1, new Vector3(player[i].transform.position.x, 1.4f, player[i].transform.position.z), Quaternion.identity);
+
+            }
+
+        }
     }
 }
--- Assets/Scrips/Player_.cs
+++ Assets/Scrips/Player_.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player_ : MonoBehaviour
 {
     public int step_move;
+    public GameObject selected1;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +16,12 @@ public class Player_ : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //GameObject cam = GameObject.Find("Directional Light");
+        //Main mScript = cam.GetComponent<Main>();
+
+        //if(step_move == mScript.get_current_move())
+        //{
+        //    Instantiate(selected1, new Vector3(transform.position.x, 2.5f, transform.position.z), Quaternion.identity);
+        //}
     }
 }
