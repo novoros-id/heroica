22be3a625cbe125ff4374a2e8fa0e9c57151a7ef
novoros-id <novoros-id@inbox.ru>
2021-06-08 23:06:01 +0300

    zoom graph

--- Assets/Scrips/Main.cs
+++ Assets/Scrips/Main.cs
@@ -10,29 +10,213 @@ public class Main : MonoBehaviour
     private int current_move = 1;
     public GameObject[] player;
     public GameObject[] pr_hod;
+    public GameObject[] floor_g;
     public GameObject selected1;
+
     public GameObject cam_focus;
     public bool Pc;
     public string lang = "en";
+    public Text TextComment;
+    public Graph g = new Graph();
+    public GameObject[] Blue;
+
+    public GameObject blue_floor;
 
     public void Start()
     {
+        
         move_priznak_step();
 
         if (Application.platform == RuntimePlatform.WindowsPlayer)
         {
             Pc = true;
         }
-        //else if(Application.isPlaying)
-        //{
-        //    Pc = true;
-        //   // Debug.Log("pc");
-        //}
         else
         {
             Pc = false;
         }
+
+        build_graph();
+        
+
     }
+
+    /// <summary>
+    /// /////// постройка графа
+    /// </summary>
+   
+
+    public void addEdgeFromFloor(string nameFloor, Graph gr)
+    {
+
+        GameObject CurFloor;
+        Vector3 CurFloorPos;
+        Collider[] colliders;
+        float x, z;
+
+        CurFloor = GameObject.Find(nameFloor);
+        CurFloorPos = new Vector3(CurFloor.transform.position.x, CurFloor.transform.position.y, CurFloor.transform.position.z);
+
+        if ((colliders = Physics.OverlapSphere(CurFloorPos, 1)).Length > 0)
+        {
+            foreach (var collider in colliders)
+            {
+                // обрабатываем только некоторые тэги
+                if (collider.tag == "Floor")
+                {
+
+                    Vector3 pos1 = collider.transform.position - CurFloorPos;
+              
+                    // так как есть небольшие смещения, приведем к 0 округления
+                    if (Mathf.Abs(pos1.x) < 0.01)
+                    {
+                        x = 0;
+                    }
+                    else
+                    {
+                        x = pos1.x;
+                    }
+
+                    if (Mathf.Abs(pos1.z) < 0.01)
+                    {
+                        z = 0;
+                    }
+                    else
+                    {
+                        z = pos1.z;
+                    }
+
+                    // add to Graph
+          
+                    if (CurFloor.name == "StartFloor")
+                    {
+                        gr.AddEdge(CurFloor.name, collider.name, 1);
+                    }
+
+                    if (x > 0 && z == 0)
+                    {
+                        gr.AddEdge(nameFloor, collider.name, 1);
+                    }
+
+                    if (x < 0 && z == 0)
+                    {
+                        gr.AddEdge(nameFloor, collider.name, 1);
+                    }
+
+                    if (z > 0 && x == 0)
+                    {
+                        gr.AddEdge(nameFloor, collider.name, 1);
+                    }
+
+                    if (z < 0 && x == 0)
+                    {
+                        gr.AddEdge(nameFloor, collider.name, 1);
+                    }
+
+                }
+            }
+        }
+    }
+
+    public void build_graph() 
+    {
+        
+
+        //добавление вершин
+
+        //не ставить тэг на это поле
+        g.AddVertex("StartFloor");
+        // остальные вершины добавляем по тэгу
+
+        floor_g = GameObject.FindGameObjectsWithTag("Floor");
+
+        for (int i = 0; i < floor_g.Length; i++)
+        {
+
+            g.AddVertex(floor_g[i].name);
+            addEdgeFromFloor(floor_g[i].name, g);
+
+        }
+
+        // добавим грани
+        addEdgeFromFloor("StartFloor", g);
+        floor_g = GameObject.FindGameObjectsWithTag("Floor");
+
+        for (int i = 0; i < floor_g.Length; i++)
+        {
+
+            addEdgeFromFloor(floor_g[i].name, g);
+
+        }
+
+    }
+
+    public void show_the_way (string starFloor,string endFloor)
+    {
+
+        build_graph();
+        clear_blue();
+
+        var dijkstra = new Dijkstra(g);
+        var path = dijkstra.FindShortestPath(starFloor, endFloor);
+
+        int price_way = return_price_way(path);
+        Debug.Log("Price way " + price_way);
+
+        string[] subs = path.Split(',');
+        foreach (var sub in subs)
+        {
+            //Debug.Log(sub);
+            GameObject floor_ = GameObject.Find(sub);
+            Instantiate(blue_floor, new Vector3(floor_.transform.position.x, 1.05f, floor_.transform.position.z), Quaternion.identity);
+        }
+
+    }
+
+    public int return_price_way (string path)
+    {
+        int cost_way = 0;
+
+        string[] subs = path.Split(',');
+        foreach (var sub in subs)
+        {
+            //Debug.Log(sub);
+            GameObject floor_ = GameObject.Find(sub);
+            // Instantiate(blue_floor, new Vector3(floor_.transform.position.x, 1.05f, floor_.transform.position.z), Quaternion.identity);
+            GameObject _items = return_tag_item_on_position(floor_.transform.position);
+            if (_items == null)
+            {
+                cost_way += 1;
+            }
+            else if (_items.tag == "Door")
+            {
+                cost_way += 1000;
+            }
+            else if (_items.tag == "Enemy_1")
+            {
+                cost_way += 2;
+            }
+            else if (_items.tag == "Enemy_2")
+            {
+                cost_way += 3;
+            }
+            else
+            {
+                cost_way += 1;
+            }
+
+          
+        }
+
+        return cost_way;
+    }
+
+
+    /// <summary>
+    /// управление ходом
+    /// </summary>
+   
+    
 
     public int get_current_move()
     {
@@ -55,7 +239,7 @@ public class Main : MonoBehaviour
 
     public void move_priznak_step(string text_add = "")
     {
-        var myText = GameObject.Find("Text_").GetComponent<Text>();
+       // var myText = GameObject.Find("Text_").GetComponent<Text>();
 
         if (text_add != "")
         {
@@ -152,7 +336,431 @@ public class Main : MonoBehaviour
 
     public void add_text(string a_text)
     {
-        var myText = GameObject.Find("Text_").GetComponent<Text>();
-        myText.text = a_text + "\n" + "\n" + myText.text;
+        //var myText = GameObject.Find("Text_").GetComponent<Text>();
+        TextComment.text = a_text + "\n" + "\n" + TextComment.text;
+    }
+
+    /// <summary>
+    /// ///////////// вспомогательные
+    /// </summary>
+
+    public void clear_blue()
+    {
+        Blue = GameObject.FindGameObjectsWithTag("Blue");
+
+        for (int b = 0; b < Blue.Length; b++)
+        {
+            Destroy(Blue[b]);
+            //Debug.Log("a");
+        }
+    }
+
+
+    public GameObject return_tag_item_on_position(Vector3 player_position)
+    {
+        //string tag_item = "";
+        Vector3 CurFloorPos;
+        Collider[] colliders;
+
+        CurFloorPos = new Vector3(player_position.x, player_position.y, player_position.z);
+
+        //Debug.Log("Текущая позиция платформы" + CurFloorPos);
+
+        if ((colliders = Physics.OverlapSphere(CurFloorPos, 0.5f)).Length > 0)
+        {
+
+            foreach (var collider in colliders)
+            {
+                // обрабатываем только некоторые тэги
+
+                if (collider.tag == "Enemy_1" ||
+                    collider.tag == "Enemy_2" ||
+                    collider.tag == "Enemy_boss" ||
+                    collider.tag == "Key" ||
+                    collider.tag == "Door" ||
+                    collider.tag == "item_blood" ||
+                    collider.tag == "item_luck" ||
+                    collider.tag == "item_speed" ||
+                    collider.tag == "item_power" ||
+                    collider.tag == "item_gold" ||
+                    collider.tag == "item_axe" ||
+                    collider.tag == "item_baton" ||
+                    collider.tag == "item_scythe" ||
+                    collider.tag == "item_bow" ||
+                    collider.tag == "item_dagger" ||
+                    collider.tag == "item_sword")
+
+                {
+
+                    return collider.gameObject;
+
+
+                }
+            }
+        }
+
+
+        //string[] tag_item = new string[] { "Key", "Door", "Enemy_1","Enemy_2","Enemy_boss","item_blood"
+        //                                  , "item_luck", "item_speed", "item_power", "item_gold", "item_axe", "item_baton"
+        //                                  , "item_scythe", "item_bow", "item_dagger", "item_sword"};
+
+        //foreach (string tag in tag_item)
+        //{
+
+        //    GameObject[] item = GameObject.FindGameObjectsWithTag(tag);
+
+        //    for (int i = 0; i < item.Length; i++)
+        //    {
+        //        if (item[i].transform.position.x == player_position.x && item[i].transform.position.z == player_position.z)
+        //        {
+        //            return item[i];
+        //        }
+        //    }
+
+
+        //}
+
+
+        return null;
+
     }
 }
+
+
+
+
+/////////////////////
+/////////////////////
+////////////////////
+/// Graph
+/// https://programm.top/c-sharp/data-structures/graph/
+///
+
+/// <summary>
+/// Ребро графа
+/// </summary>
+public class GraphEdge
+{
+    /// <summary>
+    /// Связанная вершина
+    /// </summary>
+    public GraphVertex ConnectedVertex { get; }
+
+    /// <summary>
+    /// Вес ребра
+    /// </summary>
+    public int EdgeWeight { get; }
+
+    /// <summary>
+    /// Конструктор
+    /// </summary>
+    /// <param name="connectedVertex">Связанная вершина</param>
+    /// <param name="weight">Вес ребра</param>
+    public GraphEdge(GraphVertex connectedVertex, int weight)
+    {
+        ConnectedVertex = connectedVertex;
+        EdgeWeight = weight;
+    }
+}
+
+/// <summary>
+/// Вершина графа
+/// </summary>
+public class GraphVertex
+{
+    /// <summary>
+    /// Название вершины
+    /// </summary>
+    public string Name { get; }
+
+    /// <summary>
+    /// Список ребер
+    /// </summary>
+    public List<GraphEdge> Edges { get; }
+
+    /// <summary>
+    /// Конструктор
+    /// </summary>
+    /// <param name="vertexName">Название вершины</param>
+    public GraphVertex(string vertexName)
+    {
+        Name = vertexName;
+        Edges = new List<GraphEdge>();
+    }
+
+    /// <summary>
+    /// Добавить ребро
+    /// </summary>
+    /// <param name="newEdge">Ребро</param>
+    public void AddEdge(GraphEdge newEdge)
+    {
+        Edges.Add(newEdge);
+    }
+
+    /// <summary>
+    /// Добавить ребро
+    /// </summary>
+    /// <param name="vertex">Вершина</param>
+    /// <param name="edgeWeight">Вес</param>
+    public void AddEdge(GraphVertex vertex, int edgeWeight)
+    {
+        AddEdge(new GraphEdge(vertex, edgeWeight));
+    }
+
+    /// <summary>
+    /// Преобразование в строку
+    /// </summary>
+    /// <returns>Имя вершины</returns>
+    public override string ToString() => Name;
+}
+
+/// <summary>
+/// Граф
+/// </summary>
+public class Graph
+{
+    /// <summary>
+    /// Список вершин графа
+    /// </summary>
+    public List<GraphVertex> Vertices { get; }
+
+    /// <summary>
+    /// Конструктор
+    /// </summary>
+    public Graph()
+    {
+        Vertices = new List<GraphVertex>();
+    }
+
+    /// <summary>
+    /// Добавление вершины
+    /// </summary>
+    /// <param name="vertexName">Имя вершины</param>
+    public void AddVertex(string vertexName)
+    {
+        Vertices.Add(new GraphVertex(vertexName));
+    }
+
+    /// <summary>
+    /// Поиск вершины
+    /// </summary>
+    /// <param name="vertexName">Название вершины</param>
+    /// <returns>Найденная вершина</returns>
+    public GraphVertex FindVertex(string vertexName)
+    {
+        foreach (var v in Vertices)
+        {
+            if (v.Name.Equals(vertexName))
+            {
+                return v;
+            }
+        }
+
+        return null;
+    }
+
+    /// <summary>
+    /// Добавление ребра
+    /// </summary>
+    /// <param name="firstName">Имя первой вершины</param>
+    /// <param name="secondName">Имя второй вершины</param>
+    /// <param name="weight">Вес ребра соединяющего вершины</param>
+    public void AddEdge(string firstName, string secondName, int weight)
+    {
+        var v1 = FindVertex(firstName);
+        var v2 = FindVertex(secondName);
+        if (v2 != null && v1 != null)
+        {
+            v1.AddEdge(v2, weight);
+            v2.AddEdge(v1, weight);
+        }
+    }
+}
+
+
+////////////////////
+///////////////////
+/// Алгоритм Дейкстера
+///https://programm.top/c-sharp/algorithm/search/dijkstra-algorithm/
+///
+
+/// <summary>
+/// Информация о вершине
+/// </summary>
+public class GraphVertexInfo
+{
+    /// <summary>
+    /// Вершина
+    /// </summary>
+    public GraphVertex Vertex { get; set; }
+
+    /// <summary>
+    /// Не посещенная вершина
+    /// </summary>
+    public bool IsUnvisited { get; set; }
+
+    /// <summary>
+    /// Сумма весов ребер
+    /// </summary>
+    public int EdgesWeightSum { get; set; }
+
+    /// <summary>
+    /// Предыдущая вершина
+    /// </summary>
+    public GraphVertex PreviousVertex { get; set; }
+
+    /// <summary>
+    /// Конструктор
+    /// </summary>
+    /// <param name="vertex">Вершина</param>
+    public GraphVertexInfo(GraphVertex vertex)
+    {
+        Vertex = vertex;
+        IsUnvisited = true;
+        EdgesWeightSum = int.MaxValue;
+        PreviousVertex = null;
+    }
+}
+
+/// <summary>
+/// Алгоритм Дейкстры
+/// </summary>
+public class Dijkstra
+{
+    Graph graph;
+
+    List<GraphVertexInfo> infos;
+
+    /// <summary>
+    /// Конструктор
+    /// </summary>
+    /// <param name="graph">Граф</param>
+    public Dijkstra(Graph graph)
+    {
+        this.graph = graph;
+    }
+
+    /// <summary>
+    /// Инициализация информации
+    /// </summary>
+    void InitInfo()
+    {
+        infos = new List<GraphVertexInfo>();
+        foreach (var v in graph.Vertices)
+        {
+            infos.Add(new GraphVertexInfo(v));
+        }
+    }
+
+    /// <summary>
+    /// Получение информации о вершине графа
+    /// </summary>
+    /// <param name="v">Вершина</param>
+    /// <returns>Информация о вершине</returns>
+    GraphVertexInfo GetVertexInfo(GraphVertex v)
+    {
+        foreach (var i in infos)
+        {
+            if (i.Vertex.Equals(v))
+            {
+                return i;
+            }
+        }
+
+        return null;
+    }
+
+    /// <summary>
+    /// Поиск непосещенной вершины с минимальным значением суммы
+    /// </summary>
+    /// <returns>Информация о вершине</returns>
+    public GraphVertexInfo FindUnvisitedVertexWithMinSum()
+    {
+        var minValue = int.MaxValue;
+        GraphVertexInfo minVertexInfo = null;
+        foreach (var i in infos)
+        {
+            if (i.IsUnvisited && i.EdgesWeightSum < minValue)
+            {
+                minVertexInfo = i;
+                minValue = i.EdgesWeightSum;
+            }
+        }
+
+        return minVertexInfo;
+    }
+
+    /// <summary>
+    /// Поиск кратчайшего пути по названиям вершин
+    /// </summary>
+    /// <param name="startName">Название стартовой вершины</param>
+    /// <param name="finishName">Название финишной вершины</param>
+    /// <returns>Кратчайший путь</returns>
+    public string FindShortestPath(string startName, string finishName)
+    {
+        return FindShortestPath(graph.FindVertex(startName), graph.FindVertex(finishName));
+    }
+
+    /// <summary>
+    /// Поиск кратчайшего пути по вершинам
+    /// </summary>
+    /// <param name="startVertex">Стартовая вершина</param>
+    /// <param name="finishVertex">Финишная вершина</param>
+    /// <returns>Кратчайший путь</returns>
+    public string FindShortestPath(GraphVertex startVertex, GraphVertex finishVertex)
+    {
+        InitInfo();
+        var first = GetVertexInfo(startVertex);
+        first.EdgesWeightSum = 0;
+        while (true)
+        {
+            var current = FindUnvisitedVertexWithMinSum();
+            if (current == null)
+            {
+                break;
+            }
+
+            SetSumToNextVertex(current);
+        }
+
+        return GetPath(startVertex, finishVertex);
+    }
+
+    /// <summary>
+    /// Вычисление суммы весов ребер для следующей вершины
+    /// </summary>
+    /// <param name="info">Информация о текущей вершине</param>
+    void SetSumToNextVertex(GraphVertexInfo info)
+    {
+        info.IsUnvisited = false;
+        foreach (var e in info.Vertex.Edges)
+        {
+            var nextInfo = GetVertexInfo(e.ConnectedVertex);
+            var sum = info.EdgesWeightSum + e.EdgeWeight;
+            if (sum < nextInfo.EdgesWeightSum)
+            {
+                nextInfo.EdgesWeightSum = sum;
+                nextInfo.PreviousVertex = info.Vertex;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Формирование пути
+    /// </summary>
+    /// <param name="startVertex">Начальная вершина</param>
+    /// <param name="endVertex">Конечная вершина</param>
+    /// <returns>Путь</returns>
+    string GetPath(GraphVertex startVertex, GraphVertex endVertex)
+    {
+        var path = endVertex.ToString();
+        while (startVertex != endVertex)
+        {
+            endVertex = GetVertexInfo(endVertex).PreviousVertex;
+            path = endVertex.ToString() + "," + path;
+        }
+
+        return path;
+    }
+}
+
--- Assets/Scrips/OnClick_Plat.cs
+++ Assets/Scrips/OnClick_Plat.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class OnClick_Plat : MonoBehaviour
 {
     public Vector3 curpos;
-    
+    public GameObject[] player;
+    public GameObject Curent_player;
+    public string CurFloorName;
+    public GameObject[] Blue;
+
     void Start()
     {
         curpos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
@@ -13,6 +17,79 @@ public class OnClick_Plat : MonoBehaviour
         
     void OnMouseDown()
     {
-        // xsDebug.Log(curpos);
+
+        Curent_player = return_curent_player(); // нашли текущего игркока
+        CurFloorName = Return_floor_player(new Vector3(Curent_player.transform.position.x, Curent_player.transform.position.y, Curent_player.transform.position.z));
+        // Debug.Log(name);
+
+        GameObject cam = GameObject.Find("Directional Light");
+        Main mScript = cam.GetComponent<Main>();
+        mScript.show_the_way(CurFloorName,name);
+
+    }
+
+    public GameObject return_curent_player()
+
+    {
+        player = GameObject.FindGameObjectsWithTag("Player");
+        GameObject cam = GameObject.Find("Directional Light");
+        Main mScript = cam.GetComponent<Main>();
+        int current_move = mScript.get_current_move();
+
+        for (int i = 0; i < player.Length; i++)
+        {
+
+            Player_ pl_script = player[i].GetComponent<Player_>();
+
+            if (pl_script.step_move == current_move)
+            {
+
+                return player[i];
+
+            }
+
+        }
+
+        return null;
+    }
+
+    private string Return_floor_player(Vector3 pos)
+    {
+        GameObject[] Floors;
+        float x_f = pos.x - 0;
+        float z_f = pos.z + 0;
+        string curlfloorname_ = "";
+
+        Floors = GameObject.FindGameObjectsWithTag("Floor");
+
+        for (int i = 0; i < Floors.Length; i++)
+        {
+            if (Mathf.Abs(Floors[i].transform.position.x - x_f) < 0.01 && Mathf.Abs(Floors[i].transform.position.z - z_f) < 0.01)
+            {
+                //CurFloorName = Floors[i].name;
+                //Debug.Log(Floors[i].name);
+                return Floors[i].name;
+            }
+        }
+
+        if (curlfloorname_ == "")
+        {
+            return GameObject.Find("StartFloor").name;
+
+        }
+
+        return null;
+
+    }
+
+    public void clear_blue()
+    {
+        Blue = GameObject.FindGameObjectsWithTag("Blue");
+
+        for (int b = 0; b < Blue.Length; b++)
+        {
+            Destroy(Blue[b]);
+            //Debug.Log("a");
+        }
     }
 }
