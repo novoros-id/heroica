e471997db4ece631e38c638765e0117570733ca6
Dikobrother <65683489+Dikobrother@users.noreply.github.com>
2021-06-15 20:34:13 +0300

    Ocean level

---
+++ Assets/Scrips/Ocean.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ocean : MonoBehaviour
+{
+    public GameObject[] spawns;
+    public Transform[] spawnpoints;
+
+    private int rand;
+    private int randspawns;
+    private int randPosition;
+    public float startTimeBtwSpawns;
+    private float timeBtwSpawns;
+
+    void Start()
+    {
+        timeBtwSpawns = startTimeBtwSpawns;
+    }
+
+    void Update()
+    {
+        
+        if(timeBtwSpawns <= 0)
+        {
+            rand = Random.Range(0, spawnpoints.Length);
+            randspawns = Random.Range(0, spawns.Length);
+            randPosition = Random.Range(0, spawnpoints.Length);
+            Instantiate(spawns[randspawns], spawnpoints[randPosition].transform.position, Quaternion.identity);
+            timeBtwSpawns = startTimeBtwSpawns;
+        }
+        else
+        {
+            timeBtwSpawns -= Time.deltaTime;
+        }
+
+    }
+}
---
+++ Assets/Scrips/OceanObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OceanObject : MonoBehaviour
+{
+    public float startTimeBtwDeaths = 4;
+    private float timeBtwDeaths;
+    public float roty;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeBtwDeaths = startTimeBtwDeaths;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Euler(0, roty, 0);
+        transform.position = new Vector3(transform.position.x + 0.2f, -1.2f, transform.position.z);
+        if (timeBtwDeaths <= 0)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            timeBtwDeaths -= Time.deltaTime;
+        }
+    }
+}
