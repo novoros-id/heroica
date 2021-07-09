e4e8e3dfd8e17e4c2b9a5b1bf2a09ca4497f38c4
Dikobrother <65683489+Dikobrother@users.noreply.github.com>
2021-07-09 18:35:14 +0300

    CrystalCount

---
+++ Assets/Scrips/CrystalCount.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrystalCount : MonoBehaviour
+{
+    Text Count;
+    public ClickOnCube cb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Count = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Count.text = cb.count_magic_crystall.ToString();
+    }
+}
