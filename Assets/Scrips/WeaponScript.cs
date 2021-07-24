c7c7eca221a132ec78ca367a3549eb09baa6218b
novoros-id <novoros-id@inbox.ru>
2021-07-24 20:53:15 +0300

    button

---
+++ Assets/Scrips/WeaponScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Baton()
+    {
+        Debug.Log("Baton");
+    }
+    public void Axe()
+    {
+        Debug.Log("Axe");
+    }
+    public void Scythe()
+    {
+        Debug.Log("Scythe");
+    }
+}
