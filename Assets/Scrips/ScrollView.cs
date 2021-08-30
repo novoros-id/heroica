cd86e63b642c2db6238c898cc1834c6f3388c0ab
Dikobrother <65683489+Dikobrother@users.noreply.github.com>
2021-08-30 16:35:52 +0300

    scrollview texts

---
+++ Assets/Scrips/ScrollView.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+
+public class ScrollView : MonoBehaviour
+{
+    [SerializeField] RectTransform txtRT;
+    [SerializeField] RectTransform contentRT;
+    private string lang;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("lang"))
+        {
+            lang = PlayerPrefs.GetString("lang");
+        }
+        else
+        {
+            lang = "en";
+        }
+        if(lang == "ru")
+        {
+            txtRT = GameObject.Find("level Description_ru").GetComponent<RectTransform>();
+        }
+        else
+        {
+            txtRT = GameObject.Find("level Description").GetComponent<RectTransform>();
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    { 
+        var size = contentRT.sizeDelta;
+        size.y = txtRT.sizeDelta.y;
+        contentRT.sizeDelta = size;
+    }
+}
