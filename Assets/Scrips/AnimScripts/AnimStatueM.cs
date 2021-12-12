a2848dc17302ef7308d924d065c5745f289ae3ce
Dikobrother <65683489+Dikobrother@users.noreply.github.com>
2021-12-12 20:58:28 +0300

    Some animations

---
+++ Assets/Scrips/AnimScripts/AnimStatueM.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimStatueM : MonoBehaviour
+{
+    Animator anim;
+
+    [SerializeField] private float time1;
+
+    private float _timeLeft = 0f;
+    private float _timeLeft1 = 0f;
+    private bool _timerOn = false;
+    private bool _timerOn1 = false;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.Play("Idle");
+        _timeLeft1 = time1;
+        _timeLeft = Random.Range(5, 10);
+        _timerOn = true;
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_timerOn)
+        {
+            if (_timeLeft > 0)
+            {
+                _timeLeft -= Time.deltaTime;
+            }
+            else
+            {
+                _timerOn1 = true;
+                _timeLeft = Random.Range(5, 10);
+                anim.Play("Test");
+            }
+        }
+
+
+        if (_timerOn1)
+        {
+            if (_timeLeft1 > 0)
+            {
+                _timeLeft1 -= Time.deltaTime;
+            }
+            else
+            {
+                _timeLeft1 = time1;
+                _timerOn1 = false;
+                anim.Play("Idle");
+            }
+        }
+    }
+
+}
