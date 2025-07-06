94ab15f9198193f4cabfd91aefb91a329bbcadd8
Dikobrother <vgn_v@live.ru>
2025-07-06 19:49:27 +0300

    New UI

--- Assets/Scrips/ClickOnCube.cs
+++ Assets/Scrips/ClickOnCube.cs
@@ -54,6 +54,14 @@ public class ClickOnCube : MonoBehaviour
         UI = GameObject.Find("UI");
         TextEndGame = GameObject.Find("TextEnd").GetComponent<Text>();
         CrystalButton_ = GameObject.Find("CrystalButton");
+        if (PlayerPrefs.HasKey("Delay"))
+        {
+            computerMoveDelay = PlayerPrefs.GetFloat("Delay");
+        }
+        else
+        {
+            computerMoveDelay = 0;
+        }
     }
 
     void Start()
